040dbddcad1c14801d8ea120ee2ed17cf29daa8e
tlabna <tlabna@users.noreply.github.com>
2018-03-22 19:40:59 -0400

    test: Update tests for App component

--- services/client/src/components/__tests__/App.test.js
+++ services/client/src/components/__tests__/App.test.js
@@ -1,24 +1,53 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
-import toJson from 'enzyme-to-json'
 
 import App from '../App'
+import AddUser from '../AddUser'
+import UsersList from '../UsersList'
 
-// Using jest.fn() as a mock function
-// Since only testing if component renders properly
-const options = {
-  disableLifecycleMethods: true,
-}
+describe('App Component', () => {
+  it('renders AddUser component', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(AddUser)).toHaveLength(1)
+  })
 
-test('App renders properly', () => {
-  const wrapper = shallow(<App />, options)
-  expect(wrapper.find('h1').length).toBe(1)
-  expect(wrapper.find('AddUser').length).toBe(1)
-  expect(wrapper.find('UsersList').length).toBe(1)
+  it('renders UsersList component', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(UsersList)).toHaveLength(1)
+  })
+
+  it(`renders header 'All Users'`, () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('h1').text()).toBe('All Users')
+  })
+
+  it('passes props to AddUser component', () => {
+    const wrapper = shallow(<App />)
+    let addUserWrapper = wrapper.find(AddUser)
+
+    expect(addUserWrapper.props().username).toEqual('')
+    expect(addUserWrapper.props().email).toEqual('')
+    // Checking instance because wrapper has method as instance and not prop
+    expect(addUserWrapper.props().addUser).toBe(wrapper.instance().addUser)
+    expect(addUserWrapper.props().handleChange).toBe(
+      wrapper.instance().handleChange
+    )
+
+    wrapper.setState({
+      username: 'test',
+      email: 'test@test.com',
+    })
+
+    addUserWrapper = wrapper.find(AddUser)
+    expect(addUserWrapper.props().username).toEqual('test')
+    expect(addUserWrapper.props().email).toEqual('test@test.com')
+  })
 })
 
-test('App renders a snapshot properly', () => {
-  const tree = renderer.create(<App />).toJSON()
-  expect(tree).toMatchSnapshot()
+describe('App Component Snapshot', () => {
+  test('renders', () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
