c58c530b5d3a8713731de55f29d044508099c85a
tlabna <tlabna@users.noreply.github.com>
2018-04-04 17:38:38 -0400

    feat: Add authentication route for users service
    - Register auth blueprint
    - Add tests for auth (/auth/register route)
    - Add view for auth (/auth/register route)

---
+++ services/users/project/api/auth.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, jsonify, request
+from sqlalchemy import exc, or_
+
+from project.api.models import User
+from project import db
+
+auth_blueprint = Blueprint('auth', __name__)
+
+
+@auth_blueprint.route('/auth/register', methods=['POST'])
+def register_user():
+    # get post data
+    post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.',
+    }
+
+    if not post_data:
+        return jsonify(response_object), 400
+
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
+
+    if None in (username, email, password):
+        return jsonify(response_object), 400
+
+    try:
+        # Check for existing user
+        user = User.query.filter(
+            or_(User.username == username, User.email == email)).first()
+
+        if not user:
+            # add new user to db
+            new_user = User(
+                username=username,
+                email=email,
+                password=password,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            # generate auth token
+            auth_token = new_user.encode_auth_token(new_user.id)
+            response_object['status'] = 'success'
+            response_object['message'] = 'Successfully registered.'
+            response_object['auth_token'] = auth_token.decode()
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That user already exists.'
+            return jsonify(response_object), 400
+    # handle errors
+    except (exc.IntegrityError, ValueError) as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
